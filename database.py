0cdd3d56698f14d400edc467ab790e3b494deaf0
Adel-Kayyali <102745376+Adel-Kayyali@users.noreply.github.com>
2023-05-17 15:39:12 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,23 @@
+from deta import Deta
+
+
+DETA_KEY = "a0gvexc1bqy_h5pJLaWjTz7ZTHztZGBUkfU7EfrZXafq"
+
+# Initialize with the key 
+deta = Deta(DETA_KEY)
+
+#create/connect db
+db = deta.Base("DiagnoAI_DB")
+
+def insert_data (Mean_Radius,Mean_Perimeter,Mean_Area,Mean_Concavity,Mean_Concavity_Points,Worst_Radius,Worst_Perimeter
+                ,Worst_Area,Worst_Concavity,Worst_Concavity_Points):
+    """Returns the report on a succesful creation, otherwise raises an error"""
+    return db.put({"key":Mean_Radius, "Mean_Perimeter":Mean_Perimeter, "Mean_Area":Mean_Area, "Mean_Concavity":Mean_Concavity,
+                   "Mean_Concavity_Points":Mean_Concavity_Points, "Worst_Radius":Worst_Radius, "Worst_Perimeter":Worst_Perimeter,
+                   "Worst_Area":Worst_Area, "Worst_Concavity":Worst_Concavity, "Worst_Concavity_Points":Worst_Concavity_Points})
+
+
+def fetch_all_data():
+    """Returns a dict of all data"""
+    res = db.fetch()
+    return res.items
