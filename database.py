2e9cfb95230237951fcae11baf0541cd78e3e7dd
Adel-Kayyali <102745376+Adel-Kayyali@users.noreply.github.com>
2023-05-20 23:24:51 +0300

    database.py

--- database.py
+++ database.py
@@ -12,9 +12,9 @@ db = deta.Base("DiagnoAI_DB")
 def insert_data (user_name, Mean_Radius,Mean_Perimeter,Mean_Area,Mean_Concavity,Mean_Concavity_Points,Worst_Radius,Worst_Perimeter
                 ,Worst_Area,Worst_Concavity,Worst_Concavity_Points):
     """Returns the report on a succesful creation, otherwise raises an error"""
-    return db.put({"user_name":user_name, "Mean_Radius":Mean_Radius, "Mean_Perimeter":Mean_Perimeter, "Mean_Area":Mean_Area, "Mean_Concavity":Mean_Concavity,
-                   "Mean_Concavity_Points":Mean_Concavity_Points, "Worst_Radius":Worst_Radius, "Worst_Perimeter":Worst_Perimeter,
-                   "Worst_Area":Worst_Area, "Worst_Concavity":Worst_Concavity, "Worst_Concavity_Points":Worst_Concavity_Points})
+    return db.put({"User_name":user_name, "mean_Radius":Mean_Radius, "mean_Perimeter":Mean_Perimeter, "mean_Area":Mean_Area, "mean_Concavity":Mean_Concavity,
+                   "mean_Concavity_Points":Mean_Concavity_Points, "worst_Radius":Worst_Radius, "worst_Perimeter":Worst_Perimeter,
+                   "worst_Area":Worst_Area, "worst_Concavity":Worst_Concavity, "worst_Concavity_Points":Worst_Concavity_Points})
 
 
 def fetch_all_data():
